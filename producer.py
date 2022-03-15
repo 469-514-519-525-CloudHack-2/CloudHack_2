003ada32fcde2e6ad42f6c2622c20d4392c915f1
ShreyasBaasri07 <57621491+ShreyasBaasri07@users.noreply.github.com>
2022-03-15 22:18:27 +0530

    updated producer

--- producer.py
+++ producer.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route("/new_ride", methods = ["POST"])
 def new_ride():
 	data = request.json
+
+
+@app.route("/new_ride_matching_consumer", methods = ["POST"])
+def new_ride_matching_consumer():
+	data = request.json
     
 	
 
