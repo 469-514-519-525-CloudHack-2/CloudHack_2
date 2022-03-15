120c6cee7975ac9dc83ecb935aac963bb5745853
SuhasRK <suhasrk233@gmail.com>
2022-03-15 22:25:55 +0530

    change in ride_match_comsumer.py

---
+++ ride_matching_consumer.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, jsonify
+
+
+app = Flask(__name__)
+
+
+
+@app.route("/new_ride_matching_consumer", methods = ["POST"])
+def new_ride_matching_consumer():
+	data = request.json
+    
+	
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5001, debug=True)
