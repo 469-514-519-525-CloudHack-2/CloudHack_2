bd2d5d59f81930d3b287bf4d41daa162098befc4
sumukhbhat2701 <sumukhbhat2701@gmail.com>
2022-03-15 22:14:37 +0530

    added producer skeleton code

---
+++ producer.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, jsonify
+
+
+app = Flask(__name__)
+
+@app.route("/new_ride", methods = ["POST"])
+def new_ride():
+	data = request.json
+    
+	
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
