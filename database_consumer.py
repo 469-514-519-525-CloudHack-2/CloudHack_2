6f444ac0b0af9d027b0f1b1e51889ba6c3f21f05
SuhasRK <suhasrk233@gmail.com>
2022-03-16 20:24:24 +0530

    updated database_consumer.py

--- database_consumer.py
+++ database_consumer.py
@@ -1,21 +1,24 @@
+from crypt import methods
 from matplotlib import collections
 from pymongo import MongoClient
+from flask import Flask, render_template, request, jsonify
 
-host=MongoClient("172.17.0.2")  #docker inspect 
-# host=MongoClient("mongodb")
+app = Flask(__name__)
 
-db=host["consumerDB"]
+@app.route('/addData',methods=['POST'])
+def addData():
+    host=MongoClient("172.17.0.2")  #docker inspect 
+    # host=MongoClient("mongodb")
+    db=host["consumerDB"]
+    collection=db["consumers"]
+    sample_data=request.json
+    collection.insert_one(sample_data)
+    print('Inserted into the MongoDB database!')
+    # rec_data = collection.find_one({"name":"hello"})
+    # print("Fecthed from MongoDB: ",rec_data)    
+    return 'JSON added'
 
-collection=db["consumers"]
 
-sample_data={
-    "name":"hello",
-    "hobby":"nothing"
-}
-
-
-collection.insert_one(sample_data)
-print('Inserted into the MongoDB database!')
-
-rec_data = collection.find_one({"name":"hello"})
-print("Fecthed from MongoDB: ",rec_data)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5001, debug=True)
+    print("App started Listening...")
