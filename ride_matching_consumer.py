81941ae984b44c48dc44a761330c9640dc553896
sumukhbhat2701 <sumukhbhat2701@gmail.com>
2022-03-17 22:38:59 +0530

    dockerized producer

--- ride_matching_consumer.py
+++ ride_matching_consumer.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python
+from sqlite3 import connect
 import pika, sys, os
 from dotenv import load_dotenv
+import json
+import time
 
 load_dotenv("./consumerID.env")
 
 CONSUMER_ID = os.getenv("CONSUMER_ID")
 queue_name = "consumer_queue_{}".format(CONSUMER_ID)
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
 print(queue_name)
 def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-    channel = connection.channel()
-
     channel.queue_declare(queue = queue_name)
 
     def callback(ch, method, properties, body):
-        print("Received {}".format(body))
+        print(" [x] Received %r" % body.decode())
+        time.sleep(json.loads(body.decode())["time"])
+        print("awoken")
+        ch.basic_ack(delivery_tag = method.delivery_tag)
 
-    channel.basic_consume(queue_name, callback, auto_ack = True)
+    # channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(queue_name, callback, auto_ack = False)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
@@ -26,6 +32,7 @@ if __name__ == '__main__':
         main()
     except KeyboardInterrupt:
         print('Interrupted')
+        connection.close()
         try:
             sys.exit(0)
         except SystemExit:
